c6b9046fc998d319da1f0a18d087c16c3f8bba9d
Sidney <user@SGD-MBP.local>
2016-06-09 17:08:41 -1000

    4th Day Vault

--- vault.js
+++ vault.js
@@ -1,4 +1,39 @@
 'use strict';
+
+/*  var myVault = vault();
+console.log( myVault.getValue() ); // null
+console.log( myVault.setValue('secret password', 'hunter2') );
+
+console.log( myVault.getValue('secret password') ); // hunter2
+console.log( myVault.getValue('bank account') ); // null
+
+var yourVault = vault();
+console.log( myVault.getValue('secret password') ); // null */
+
+//THIS IS THE VAULT
 module.exports = function() {
-  
-};
+
+    var account = {};
+
+    function _setValue(daKey,value){
+      account[daKey] = value;
+      console.log(account[daKey]);
+      return account[daKey];
+    }
+      
+    function _getValue(key) {
+      if(!key||!account[key]){
+        return null;
+      }
+
+      else{
+        console.log("You are at else");
+        return account[key];
+        }
+    }
+    
+    return {
+      getValue : _getValue,
+      setValue : _setValue
+    };
+  };
